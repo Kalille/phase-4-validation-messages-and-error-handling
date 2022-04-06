95fd96f952f411eb58f8a130cf3ecb1624de47df
kalille Overton <>
2022-04-06 13:08:02 -0400

    completed"

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,9 +6,23 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+    render json: movie 
+    else
+        render json: {errors: " movie not found"}, status: :not_found
+    end
+  end
+
   def create
+   
     movie = Movie.create(movie_params)
+if movie
     render json: movie, status: :created
+else
+    render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+end
   end
 
   private
